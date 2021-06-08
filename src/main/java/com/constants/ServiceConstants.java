c5e2cb98736dc8b00d334e3386bebf1a4a34a604
teamservices <amitkumar05792@gmail.com>
2021-06-08 23:31:43 +0530

    created service constants
    created service constants

---
+++ src/main/java/com/constants/ServiceConstants.java
@@ -0,0 +1,22 @@
+/**
+ * Created on Jun 08, 2021 
+ */
+package com.constants;
+
+/**
+ * ServiceConstants contains constants used in service
+ *
+ * @author <a href="mailto:amit.kumarn@gmail.com">Amit Kumar</a>
+ */
+public class ServiceConstants {
+
+	/** Constant for employee endpoint */
+	public static final String EMPLOYEE_ENDPOINT = "/employee";
+
+	/** Constant for get employee api endpoint */
+	public static final String GET_EMPLOYEE_ENDPOINT = "/getEmployees";
+
+	/** Constant for get employee api endpoint */
+	public static final String SAVE_EMPLOYEE_ENDPOINT = "/saveEmployee";
+
+}
